680a84db796eb6cd65a65dca62701747a2e5f8a3
Digital Arts <digitalarts@bcdm52557.local>
2017-11-09 19:10:24 -0500

    Baselines finished

--- Task 3/task3_baselines.py
+++ Task 3/task3_baselines.py
@@ -10,12 +10,16 @@ import random
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
 
 def two_random_num(syns):
-    x = random.randint(0, len(syns)-1)
-    y = random.randint(0, len(syns)-1)
-    if x == y:
-        two_random_num(syns)
+    if len(syns) == 2:
+        x = 0
+        y = 1
+    else:
+        x = random.randint(0, len(syns)-1)
+        r = list(range(0,x)) + list(range(x+1, len(syns)-1))
+        y = random.choice(r)
     return x, y
 
 def get_random_senses():
@@ -24,20 +28,29 @@ def get_random_senses():
             for i in range(len(child)):
                 if child[i].attrib['senses'] == "2":
                     word_id = child[i].attrib['id']
-                    word = wordnet_lemmatizer.lemmatize(child[i].text.lower())
+                    # special cases
+                    if child[i].text.lower() == 'serves':
+                        word = 'serve'
+                    else:
+                        word = wordnet_lemmatizer.lemmatize(child[i].text.lower())
                     synsets = wn.synsets(word)
+                    if len(synsets) < 2:
+                        # special case
+                        if child[i].text.lower() == 'mothballed':
+                            word = 'mothball'
+                        else:
+                            word = porter_stemmer.stem(child[i].text.lower())
+                        synsets = wn.synsets(word)
 
                     # Get two senses of word
-                    sense_1, sense2 = two_random_num(synsets)
+                    sense_1, sense_2 = two_random_num(synsets)
                     rand_sense_1 = synsets[sense_1]
-                    lemma_1 = rand_sense_1.name().split(sep='.')[0]
-                    rand_sense_1 = rand_sense_1.name() + '.' + lemma_1
-                    rand_sense_1_lemma = wn.lemma(rand_sense_1)
-                    
-                    rand_sense_2 = synsets[sense2]
-                    lemma_2 = rand_sense_2.name().split(sep='.')[0]
-                    rand_sense_2 = rand_sense_2.name() + '.' + lemma_2
-                    rand_sense_2_lemma = wn.lemma(rand_sense_2)
+                    rand_sense_1_lemmas = wn.synset(rand_sense_1.name()).lemmas()
+                    rand_sense_1_lemma = rand_sense_1_lemmas[random.randint(0, len(rand_sense_1_lemmas)-1)]
+
+                    rand_sense_2 = synsets[sense_2]
+                    rand_sense_2_lemmas = wn.synset(rand_sense_2.name()).lemmas()
+                    rand_sense_2_lemma = rand_sense_2_lemmas[random.randint(0, len(rand_sense_2_lemmas)-1)]
 
                     sense_prediction = word_id + " " + rand_sense_1_lemma.key() + " " + rand_sense_2_lemma.key() + "\n"
                     file.write(sense_prediction)
@@ -48,5 +61,6 @@ if __name__ == "__main__":
     tree = ET.parse('../sample/subtask3-homographic-test.xml')
     root = tree.getroot()
     wordnet_lemmatizer = WordNetLemmatizer()
+    porter_stemmer = PorterStemmer()
 
     get_random_senses()
